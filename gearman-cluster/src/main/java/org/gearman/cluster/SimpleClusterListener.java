1b453177607a0db71342e8e467f77ac3d48bda90
John Ewart <john@unixninjas.org>
2013-04-28 21:34:13 -0700

    Initial branch for using Akka clustering library to add clustering support

---
+++ gearman-cluster/src/main/java/org/gearman/cluster/SimpleClusterListener.java
@@ -0,0 +1,46 @@
+package org.gearman.cluster;
+
+import akka.actor.UntypedActor;
+import akka.cluster.ClusterEvent.ClusterDomainEvent;
+import akka.cluster.ClusterEvent.CurrentClusterState;
+import akka.cluster.ClusterEvent.MemberUp;
+import akka.cluster.ClusterEvent.MemberRemoved;
+import akka.cluster.ClusterEvent.UnreachableMember;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+public class SimpleClusterListener extends UntypedActor {
+    LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+
+    public SimpleClusterListener(String identifier)
+    { }
+
+    @Override
+    public void onReceive(Object message) {
+        if (message instanceof CurrentClusterState) {
+            CurrentClusterState state = (CurrentClusterState) message;
+            log.info("Current members: {}", state.members());
+
+        } else if (message instanceof MemberUp) {
+            MemberUp mUp = (MemberUp) message;
+            log.info("Member is Up: {}", mUp.member());
+
+        } else if (message instanceof UnreachableMember) {
+            UnreachableMember mUnreachable = (UnreachableMember) message;
+            log.info("Member detected as unreachable: {}", mUnreachable.member());
+
+        } else if (message instanceof MemberRemoved) {
+            MemberRemoved mRemoved = (MemberRemoved) message;
+            log.info("Member is Removed: {}", mRemoved.member());
+
+        } else if (message instanceof ClusterDomainEvent) {
+            // ignore
+
+        } else {
+            unhandled(message);
+        }
+
+    }
+
+
+}
